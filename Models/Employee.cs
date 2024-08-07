f01f9370064ef57428270a9ccb08b77360df7d7c
Cajetan Ngede <134719269+cngede@users.noreply.github.com>
2024-08-06 23:43:47 -0400

    Uploaded models

---
+++ Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesManagement.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string EmpNo { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        public string FirstName { get; set; }
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        public string LastName { get; set; }
+        [StringLength(100, MinimumLength = 4)]
+        [Required]
+        public String FullName => "${FirstName}{MiddleName}{LastName}";
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string Phone { get; set; }
+        [Display(Name = "Address")]
+        [Required]
+        public string Address { get; set; }
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
+        public string Department { get; set; }
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
+        public string CreatedById { get; set; }
+
+    }
+}
---
+++ Models/UserActivity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesManagement.Models
+{
+    
+    public class UserActivity
+    {
+        public String? CreatedById { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        [StringLength(50, MinimumLength = 2)]
+        
+        public String? ModifiedById { get; set; }
+        
+        public DateTime ModifiedOn { get; set; }
+
+    }
+
+
+}
